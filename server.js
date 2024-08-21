3428aca8f0526c79d6a87d70aa6ab960c3087212
deepakswins <deepakswins@gmail.com>
2024-08-21 14:54:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var http = require('http');
 http.createServer(function (req, res) {
     res.write("Hello from Node.js server deepak sharma 111");
     res.end();
-}).listen(5000, function() {
-    console.log("Server is listening on port 5000");
+}).listen(5000, '139.59.36.218', function() {
+    console.log("Server is listening on 139.59.36.218:5000");
 });
